09d8794292dfc3390643824c8f45828a6d0dca89
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-07-12 20:49:21 +0530

    Create Day 00.js

---
+++ Day 00.js
@@ -0,0 +1,27 @@
+// Overview: 10 Days of JavaScript
+// This series focuses on learning and practicing JavaScript. Each challenge comes with a tutorial article, and you can view these articles by clicking either the Topics tab along the top or the article icon in the right-hand menu.
+
+// Objective
+
+// In this challenge, we review some basic concepts that will get you started with this series. Check out the tutorial to learn more about JavaScript's lexical structure.
+
+// Task
+
+// A greeting function is provided for you in the editor below. It has one parameter, parameterVariable. Perform the following tasks to complete this challenge:
+
+// Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
+// Use console.log() to print the contents of parameterVariable (i.e., the argument passed to main).
+// You've got this!
+
+/**
+*   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
+*   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+*
+*	Parameter:
+*   parameterVariable - A string of text.
+**/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+
