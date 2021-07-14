8565ce7885f2a1e212498e1a6160823b929e939c
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-07-14 22:53:11 +0530

    Create Day 1: Arithmetic Operators.js

---
+++ Day 1: Arithmetic Operators.js
@@ -0,0 +1,27 @@
+// Objective
+
+// In this challenge, we practice using arithmetic operators. Check out the attached tutorial for resources.
+
+// Task
+
+// Complete the following functions in the editor below:
+
+// getArea(length, width): Calculate and return the area of a rectangle having sides length and width.
+// getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides length and width.
+// The values returned by these functions are printed to stdout by locked stub code in the editor.
+
+
+
+function getArea(length, width) {
+    let area = length*width;
+ 
+    
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter = 2*(length+width);
+
+    
+    return perimeter;
+}
