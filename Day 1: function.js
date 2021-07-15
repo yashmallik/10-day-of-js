13700030e6209d4d44d3ea954f18da6b4d70e58e
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-07-15 23:20:17 +0530

    Create Day 1: function.js

---
+++ Day 1: function.js
@@ -0,0 +1,34 @@
+// Objective
+
+// Today, we're discussing JavaScript functions. Check out the attached tutorial for more details.
+
+// Task
+
+// Implement a function named factorial that has one parameter: an integer, n. It must return the value of n! (i.e., n factorial).
+
+// Input Format
+
+// Locked stub code in the editor reads a single integer, n, frnom stdin and passes it to a function named factorial
+
+
+
+
+function factorial(n) {
+    let v = 1
+    while (n !== 0){
+        v *= n;
+        n--
+        }
+    return v
+
+}
+
+
+
+
+
+
+
+
+
+
