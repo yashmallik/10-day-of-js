9d3f093b4d0f530afdcc693afa41a73c5bd53c06
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-07-20 23:59:22 +0530

    Update Day 03: Array.js

--- Day 03: Array.js
+++ Day 03: Array.js
@@ -1,14 +1,34 @@
+// Objective
+
+// In this challenge, we learn about Arrays. Check out the attached tutorial for more details.
+
+// Function Description
+
+// Complete the getSecondLargest function in the editor below.
+
+// getSecondLargest has the following parameters:
+
+// int nums[n]: an array of integers
+ 
+// Returns
+
+// int: the second largest number in nums
+
+
 function getSecondLargest(nums) {
     // Complete the function
     let max = nums[0];
     let secondMax = nums[1]
     if(nums[0] < nums[1]){
-        max = nums[0];
-        secondMax = nums[1] }
+        max = secondMax;
+        secondMax = nums[0] }
     for(let i = 0; i<nums.length;i++){
         if (max < nums[i]){
             secondMax = max;
             max = nums[i];
+        }
+        if (secondMax < nums[i] && max != nums[i]){
+            secondMax = nums[i]
         }
     }
     return secondMax
