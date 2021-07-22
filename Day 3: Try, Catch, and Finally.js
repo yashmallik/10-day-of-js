a5ae8218c02628b0f21781090a9c3759d305e165
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-07-22 12:22:08 +0530

    Update Day 3: Try, Catch, and Finally.js

--- Day 3: Try, Catch, and Finally.js
+++ Day 3: Try, Catch, and Finally.js
@@ -5,10 +5,24 @@
 // Task
 
 // Complete the reverseString function; it has one parameter, s. You must perform the following actions:
-
 // Try to reverse string  using the split, reverse, and join methods.
 // If an exception is thrown, catch it and print the contents of the exception's message on a new line.
 // Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
 // Input Format
 
 // Locked stub code in the editor reads variable s from stdin and passes it to the function.
+
+
+
+function reverseString(s) {
+    try{
+        s = s.split("").reverse().join("")
+    }
+    catch(c){
+        console.log(c.message)
+    }
+    finally{
+        console.log(s)
+        return
+    }
+}
