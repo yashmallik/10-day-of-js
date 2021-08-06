21ebe2dff5fa76929325c415aea346dd2c1c7452
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-08-06 16:55:43 +0530

    Create Day 7: Regular Expression III.js

---
+++ Day 7: Regular Expression III.js
@@ -0,0 +1,18 @@
+// Task
+
+// Complete the function in the editor below by returning a RegExp object, re, that matches every integer in some string s.
+
+// Constraints
+
+// The length of string s is >= 3.
+// It's guaranteed that string s contains at least one integer.
+// Output Format
+
+// The function must return a RegExp object that matches every integer in some string s.
+
+function regexVar() {
+   
+    const re = /\d+/g
+    
+    return re;
+}
