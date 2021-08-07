647c200045ed5dea506bdc78daf8d843a903f44a
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-08-07 22:49:32 +0530

    Create Day 8: Create a Button.js

---
+++ Day 8: Create a Button.js
@@ -0,0 +1,14 @@
+// Objective
+
+// In this challenge, we practice creating buttons in JavaScript. Check out the attached tutorial for learning materials.
+
+// Task
+
+// Complete the code in the editor so that it creates a clickable button satisfying the following properties:
+
+// The button's id is btn.
+// The button's initial text label is . After each click, the button must increment by . Recall that the button's text label is the JS object's innerHTML property.
+// The button has the following style properties:
+// A width of 96px.
+// A height of 48px.
+// The font-size attribute is 24px.
